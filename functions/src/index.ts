56f753268c9bc2a847b0c751c8729e64bcc83f24
taq-fujihara <taq.fujihara@gmail.com>
2020-01-23 13:00:02 +0900

    タグに属するブックマークカウントを記録するようにしてみた

---
+++ functions/src/index.ts
@@ -0,0 +1,37 @@
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const updateTagOnBookmarkWrite = functions.firestore
+  .document("users/{userId}/bookmarks/{bookmarkId}")
+  .onWrite(async (change, context) => {
+    const userId = context.params.userId;
+    const tagsBefore = change.before.data() ? change.before.data()!.tags : [];
+    const tagsAfter = change.after.data() ? change.after.data()!.tags : [];
+    const tagsInvolved: Array<string> = Array.from(
+      new Set([...tagsBefore, ...tagsAfter])
+    );
+
+    const updates = tagsInvolved.map(tag =>
+      db
+        .collection(`users/${userId}/bookmarks`)
+        .where("tags", "array-contains", tag)
+        .get()
+        .then(snapshot =>
+          db.doc(`users/${userId}/tags/${tag}`).set({
+            bookmarkCount: snapshot.size,
+            tagName: tag
+          })
+        )
+    );
+
+    await Promise.all(updates);
+  });
+
+export const updateTag = functions.firestore
+  .document("users/{userId}/tags/{tagId}")
+  .onCreate((snapshot, context) =>
+    snapshot.ref.update({ createdAt: new Date() })
+  );
